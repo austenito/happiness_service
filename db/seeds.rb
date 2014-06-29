efbdb185775afb491bac50194cbc9344da14b413
Austen Ito <austen.ito@gmail.com>
2014-06-29 12:01:31 -0400

    Only load seeds if there are no datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,29 @@
-questions = YAML.load_file('config/questions.yml')
 
-questions.each do |question_hash|
-  text = question_hash['question']
-  type = question_hash['type']
-  always_show = question_hash['always_show']
+if Question.count == 0
+  puts "Loading seed questions"
 
-  question = Question.create(text: text, question_type: type, always_show: always_show)
-  case type
+  questions = YAML.load_file('config/questions.yml')
+  questions.each do |question_hash|
+    text = question_hash['question']
+    type = question_hash['type']
+    always_show = question_hash['always_show']
+
+    question = Question.create(text: text, question_type: type, always_show: always_show)
+    case type
     when "boolean"
       question.responses = [true, false]
     when "range"
       question.responses = [question_hash['min'], question_hash['max']]
     when "multiple"
       question.responses = question_hash['responses']
+    end
+    question.freeform = true if question_hash['freeform'] == true
+    question.save!
   end
-  question.freeform = true if question_hash['freeform'] == true
-  question.save!
+else
+  puts "Questions already loaded"
 end
 
-ApiClient.create(name: 'test', token: 'testing')
+if Rails.env.development? || Rails.env.test?
+  ApiClient.create(name: 'test', token: 'testing')
+end
