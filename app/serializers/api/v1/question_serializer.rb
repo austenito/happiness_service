b9518dd191170e7f2a6e84d37710a46ae74eb2e9
Austen Ito <austen.ito@gmail.com>
2014-09-08 08:54:01 -0400

    Add fields to question serialization

--- app/serializers/api/v1/question_serializer.rb
+++ app/serializers/api/v1/question_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :question_type, :text
+  attributes :id, :question_type, :text, :responses, :freeform
   self.root false
 end
