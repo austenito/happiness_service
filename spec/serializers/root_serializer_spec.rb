3033e943a105861a318459eb5ec2c8d9b8a079e8
Austen Ito <austen.ito@gmail.com>
2015-01-01 15:37:03 -0500

    Add root seralizer spec

---
+++ spec/serializers/root_serializer_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe Api::V1::RootSerializer do
+  include Rails.application.routes.url_helpers
+
+  before do
+    Rails.application.routes.default_url_options[:host] = 'example.comm'
+  end
+
+  it 'returns root url' do
+    serializer = Api::V1::RootSerializer.new(Object.new)
+    expect(serializer.as_json).to match(
+      _links: a_hash_including(
+        self: { href: root_url }
+      )
+    )
+  end
+
+  it 'returns surveys url' do
+    serializer = Api::V1::RootSerializer.new(Object.new)
+    expect(serializer.as_json).to match(
+      _links: a_hash_including(
+        surveys: { href: "#{api_surveys_url}{/id}{?query*}" }
+      )
+    )
+  end
+
+  it 'returns users url' do
+    serializer = Api::V1::RootSerializer.new(Object.new)
+    expect(serializer.as_json).to match(
+      _links: a_hash_including(
+        users: { href: "#{api_users_url}{/id}{?query*}" }
+      )
+    )
+  end
+
+  it 'returns questions url' do
+    serializer = Api::V1::RootSerializer.new(Object.new)
+    expect(serializer.as_json).to match(
+      _links: a_hash_including(
+        questions: { href: "#{api_questions_url}{/id}{?query*}" }
+      )
+    )
+  end
+
+  it 'returns survey questions url' do
+    serializer = Api::V1::RootSerializer.new(Object.new)
+    expect(serializer.as_json).to match(
+      _links: a_hash_including(
+        survey_questions: { href: "#{api_questions_url}{/question_id}/survey_questions{?query*}" }
+      )
+    )
+  end
+end
