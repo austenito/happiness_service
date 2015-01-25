805030e799929eaeaa33ff71c260f7440127f992
Austen Ito <austen.ito@gmail.com>
2015-01-25 14:02:27 -0500

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
 def build_question(question_hash)
   text = question_hash['question']
   type = question_hash['type']
-  absolute_index = question_hash['absolute_index']
   key = question_hash['key']
 
-  question = Question.create(text: text, question_type: type, absolute_index: absolute_index, key: key)
+  question = Question.create(text: text, question_type: type, key: key)
   case type
   when "boolean"
     question.responses = [true, false]
@@ -13,7 +12,6 @@ def build_question(question_hash)
   when "multiple"
     question.responses = question_hash['responses']
   end
-  question.freeform = true if question_hash['freeform'] == true
   question.save!
   question
 end
@@ -24,14 +22,6 @@ if Question.count == 0
   questions = YAML.load_file('config/questions.yml')
   questions.each do |question_hash|
     question = build_question(question_hash)
-
-    related_questions = question_hash['related_questions']
-    if related_questions
-      related_questions.each do |related_question_hash|
-        related_question = build_question(related_question_hash)
-        related_question.update_attributes(parent_question_id: question.id)
-      end
-    end
   end
 else
   puts "Questions already loaded"
